cfa661af4db07ca072b4907adb71dbdf0f97a096
aybars yazici <aybarsyazici@sabanciuniv.edu>
2020-09-30 14:48:03 +0300

    build(init): initilized the project and added some simple examples

---
+++ src/components/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+//Stateless Component
+
+function MyComponent(props) {
+  return <h1>Hello {props.name}</h1>;
+}
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import MyComponent from "./components/MyComponent";
+import StatefulComponent from "./components/StatefulComponent";
+import CounterExample from "./components/CounterExample";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MyComponent name="Aybars" />
+    <StatefulComponent name="Aybars" />
+    <CounterExample />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
